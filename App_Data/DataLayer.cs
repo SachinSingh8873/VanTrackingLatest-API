40eef5fc14d6adeba8ba0c61eb159e22872b0377
Sachin <sachinkr887368@gmail.com>
2025-04-09 19:09:43 +0530

    Initial clean commit with proper .gitignore

---
+++ App_Data/DataLayer.cs
@@ -0,0 +1,360 @@
+﻿using System.Data;
+using Microsoft.CodeAnalysis;
+using Microsoft.Data.SqlClient;
+using VanDriverAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Emit;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Azure;
+using System.Xml.Linq;
+using System.Reflection;
+using Microsoft.Data.SqlClient.DataClassification;
+
+namespace VanDriverAPI.App_Data
+{
+    public class DataLayer
+    {
+        private readonly IConfiguration _configuration;
+
+        private SqlConnection mobjConn;
+        private string mstrConnString;
+        private int mintCmdTimeOut;
+        private string mstrErr;
+        private string mstrErrSource;
+
+
+        public string ConnectionString
+        {
+            get
+            {
+                return mstrConnString;
+            }
+            set
+            {
+                mstrConnString = value;
+            }
+        }
+
+
+        public DataLayer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var con = _configuration["ConnectionStrings:DBCon"];
+
+            //Console.WriteLine(con);
+            if (con != null)
+            {
+                mstrConnString = con;
+            }
+            else
+            {
+                mstrConnString = null;
+            }
+        }
+
+        public DataLayer(string connString)
+        {
+            ConnectionString = connString;
+            mstrConnString = connString;
+        }
+
+        public SqlConnection GetConnection(string strConn)
+        {
+            string strFunctionName = "GetConnection";
+            try
+            {
+                if (mobjConn == null || mobjConn.State != System.Data.ConnectionState.Open)
+                {
+                    mobjConn = new SqlConnection(strConn);
+                    mobjConn.Open();
+                }
+
+                return mobjConn;
+            }
+            catch (Exception ex)
+            {
+                mstrErr = ex.ToString();
+                throw new Exception(strFunctionName + " : " + mstrErr);
+            }
+            finally
+            {
+                mobjConn.Close();
+
+            }
+        }
+        public DataSet GetFunctionaryType(int code, int coded1)
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+
+                using (SqlCommand command = new SqlCommand("select ManPowerCode,  Designation from View_MasterManPower where manpowercode='" + code + "' or manpowercode='" + coded1 + "'  ORDER BY ManPowerCode", conn))
+                {
+                    try
+                    {
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                        dataAdapter.Fill(ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        ds = null;
+                    }
+                }
+            }
+            return ds;
+        }
+
+        public DataSet LoginCredentials(string loginId, string password, string examSession, string ipAddress)
+        {
+            DataSet ds = new DataSet();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("React_ReplyPerforma_Login", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@uniqueId", loginId);
+                    command.Parameters.AddWithValue("@ExamyearSession", examSession);
+                    command.Parameters.AddWithValue("@IpLocation", ipAddress);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    try
+                    {
+                        conn.Open(); // Ensure the connection is opened
+                        using (SqlDataAdapter dataAdapter = new SqlDataAdapter(command))
+                        {
+                            dataAdapter.Fill(ds);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // Log the error (consider using a logging framework)
+                        Console.WriteLine($"Error: {ex.Message}");
+                        // Optionally, you can throw the exception or handle it as needed
+                    }
+                }
+            }
+
+            return ds; // Return the DataSet (could be empty if an error occurred)
+        }
+
+
+        public DataSet React_MasterCity()
+        {
+
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("React_MasterCity", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    try
+                    {
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                        dataAdapter.Fill(ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        ds = null;
+                    }
+                }
+            }
+            return ds;
+
+        }
+
+        public DataSet React_MasterCheckEmail(string email)
+        {
+
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("React_MasterCheckEmail", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Email", email);
+
+
+                    try
+                    {
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                        dataAdapter.Fill(ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        ds = null;
+                    }
+                }
+            }
+            return ds;
+        }
+
+        public DataSet React_Validate(string Email, string Mobile, int ExamYear, string ExamSession)
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("React_Validate", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ExamYear", ExamYear);
+                    cmd.Parameters.AddWithValue("@ExamSession", ExamSession);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+                    cmd.Parameters.AddWithValue("@Mobile", Mobile);
+                    try
+                    {
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                        dataAdapter.Fill(ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error : {ex.Message}");
+                        ds = null;
+                    }
+                }
+            }
+            return ds;
+        }
+
+
+        public DataSet React_MasterState(string statecode)
+        {
+
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("React_MasterState", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@statecode", statecode);
+
+
+                    try
+                    {
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                        dataAdapter.Fill(ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        ds = null;
+                    }
+                }
+            }
+            return ds;
+
+        }
+
+        public DataSet React_MasterTitle(string title)
+        {
+
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("React_MasterTitle", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Title", title);
+
+
+                    try
+                    {
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                        dataAdapter.Fill(ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        ds = null;
+                    }
+                }
+            }
+            return ds;
+
+        }
+        public DataSet React_ExamSession()
+        {
+
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("React_ExamSession", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    try
+                    {
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                        dataAdapter.Fill(ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        ds = null;
+                    }
+                }
+            }
+            return ds;
+
+        }
+
+        public DataSet React_Master_Examination_sendEmail()
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("React_Master_Examination_Email", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    try
+                    {
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                        dataAdapter.Fill(ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        ds = null;
+                    }
+                }
+            }
+            return ds;
+        }
+
+
+        public DataSet React_ForgotPassword(string Input, string ExamSession)
+        {
+
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("React_ForgotPassword", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Input", Input);
+                    command.Parameters.AddWithValue("@ExamyearSession", ExamSession);
+
+
+                    try
+                    {
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                        dataAdapter.Fill(ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        ds = null;
+                    }
+                }
+            }
+            return ds;
+
+        }
+
+
+
+    }
+}
